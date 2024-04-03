f207c0539d33182a42c24ccd18a21932d5c565ee
Elektorn97 <elektron@Daniele-Legion>
2024-04-03 16:07:24 +0200

    bug fixed

--- scripts/main.py
+++ scripts/main.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 import rospy
-#from ros_pressure_interface.pressure_interface import Pressure_Interface
+from ros_pressure_interface.pressure_interface import Pressure_Interface
 
 def main():
-	rospy.loginfo("Ciao")
 	rospy.init_node("control_box_interface", anonymous=True)
-	# Pressure_Interface()
+	Pressure_Interface()
 	rospy.spin()
 
 if __name__ == '__main__':
